fb082598eca27db2b6ee15cf962aa3292e3e3db0
Alexey Aleshkov <djmadcat@gmail.com>
2017-01-15 16:20:47 +0300

    Add function stubs for fx

---
+++ doom2d/fx.c
@@ -0,0 +1,41 @@
+#include "fx.h"
+
+static void setamp(int a) {
+}
+
+void FX_trans1(int t) {
+}
+
+static void init_fx1sin(void) {
+}
+
+void FX_savegame(int h) {
+}
+
+void FX_loadgame(int h) {
+}
+
+void FX_alloc(void) {
+}
+
+void FX_init(void) {
+}
+
+void FX_act(void) {
+}
+
+void FX_draw(void) {
+}
+
+static int findfree(void) {
+    return 0;
+}
+
+void FX_tfog(int x, int y) {
+}
+
+void FX_ifog(int x, int y) {
+}
+
+void FX_bubble(int x, int y, int xv, int yv, int n) {
+}
---
+++ doom2d/fx.h
@@ -0,0 +1,24 @@
+#ifndef DOOM2D_FX_H
+#define DOOM2D_FX_H
+
+void FX_trans1(int t);
+
+void FX_savegame(int h);
+
+void FX_loadgame(int h);
+
+void FX_alloc(void);
+
+void FX_init(void);
+
+void FX_act(void);
+
+void FX_draw(void);
+
+void FX_tfog(int x, int y);
+
+void FX_ifog(int x, int y);
+
+void FX_bubble(int x, int y, int xv, int yv, int n);
+
+#endif //DOOM2D_FX_H
