0fc51950f49be09f752d8068aba00ac488cdf22e
Alexey Aleshkov <djmadcat@gmail.com>
2017-01-15 16:24:13 +0300

    Add function stubs for a8

---
+++ doom2d/a8.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include "a8.h"
+
+static signed char *unpack(char *d, signed char *p, int l) {
+    return NULL;
+}
+
+static unsigned char *draw(signed char *p) {
+    return NULL;
+}
+
+static unsigned char *fill(signed char *p) {
+    return NULL;
+}
+
+static unsigned char *draw2c(signed char *p) {
+    return NULL;
+}
+
+static unsigned char *draw2(signed char *p) {
+    return NULL;
+}
+
+static void f_close(void) {
+}
+
+void A8_close(void) {
+}
+
+int A8_nextframe(void) {
+    return 0;
+}
+
+static void wait_scr(int s) {
+}
+
+static void blank_scr(void) {
+}
+
+int A8_start(const char *nm) {
+    return 0;
+}
---
+++ doom2d/a8.h
@@ -0,0 +1,10 @@
+#ifndef DOOM2D_A8_H
+#define DOOM2D_A8_H
+
+void A8_close(void);
+
+int A8_nextframe(void);
+
+int A8_start(const char *nm);
+
+#endif //DOOM2D_A8_H
